df902611c49c0bdabc7b25b2d676423cb3250b13
Andi <Andi@192.168.1.31>
2018-03-27 19:04:38 +0200

    changed Table creation for components
    Added Table creation for failure modes
    Added method for adding components to the data base

--- FMEDATÜV/src/sqlite/SqlComponentHandler.java
+++ FMEDATÜV/src/sqlite/SqlComponentHandler.java
@@ -6,25 +6,30 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DecimalFormat;
 
-public class SQLiteTest {
+import core.Component;
+import core.FailureMode;
+
+public class SqlComponentHandler {
 
 	
-	private static Connection con;
+	private static Connection connection;
 	private static boolean hasData = false;
+	
 	
 	
 	public ResultSet displayComponents() throws ClassNotFoundException, SQLException {
 		
-		if(con == null) {
+		if(connection == null) {
 			getConnection();
 		}
 		
-		Statement state = con.createStatement();
+		Statement state = connection.createStatement();
 		System.out.println("HIER KOMMT DER USER");
-		ResultSet res = state.executeQuery("SELECT compType FROM components");
+		ResultSet resultSet = state.executeQuery("SELECT compType FROM components");
 		
-		return res;
+		return resultSet;
 	}
 
 
@@ -34,7 +39,7 @@ public class SQLiteTest {
 	private void getConnection() throws ClassNotFoundException, SQLException {
 		
 		Class.forName("org.sqlite.JDBC");
-		con = DriverManager.getConnection("jdbc:sqlite:Components.db");
+		connection = DriverManager.getConnection("jdbc:sqlite:Components.db");
 		initialise();
 	}
 
@@ -44,53 +49,80 @@ public class SQLiteTest {
 	private void initialise() throws SQLException {
 		// TODO Auto-generated method stub
 		if(hasData == false) {
-			
 			hasData = true;
-			
-			Statement state = con.createStatement();
+			Statement statement = connection.createStatement();
 			try{
-				ResultSet res = state.executeQuery("SELECT compType FROM components");
-				if(res.next()) {
+				ResultSet result = statement.executeQuery("SELECT compType FROM components");
+				if(result.next()) {
 					System.out.println("Tabelle schon vorhanden");
 					return;
 				}
 			}
 			catch (Exception e) {
 				System.out.println("ICH BIN DIE EXCEPTION");
-			}
+				}
+			
+			
+			//If no table is found, create table components
+			
 			
 				System.out.println("Keine Componenten Tabelle gefunden, erstelle neue Tabelle");
-				Statement state2 = con.createStatement();
-				state2.execute("CREATE TABLE components(id integer," + "compNumber integer,"
-						+ "compType varchar(60)," + "compFunction varchar(60),"
-						+ "primary key(id));");
-				System.out.println("Tabel erstellt");
+				Statement createComponentstable = connection.createStatement();
+				createComponentstable.execute(
+						"CREATE TABLE components"
+						+ "(id integer,"
+						+ "compNumber integer,"
+						+ "compType varchar(90),"
+						+"compFunction varchar(90),"
+						+"primary key(id));"
+						);
 				
-//				inserting some data
-				System.out.println("Inserting Dummy Data");
-				PreparedStatement prep = con.prepareStatement("INSERT INTO components values(?,?,?,?);");
-				prep.setInt(2, 1);
-				prep.setString(3, "Optokoppler");
-				prep.setString(4, "Unterbrechung Sender");
-				prep.execute();
+				System.out.println("components Tabelle erstellt");
+				
+				
+				
+			//If no table is found, create table failuremodes
+				
+				
+				Statement createFailuremodestable = connection.createStatement();
+				createFailuremodestable.execute(
+						"CREATE TABLE failuremodes"
+						+ "(id integer,"
+						+ "isPartOfComponent varchar(90),"
+						+ "failureModeDescription varchar(200)," 
+						+ "overALlProportion DOUBLE,"
+						+ "measures varchar(200),"
+						+ "dc DOUBLE,"
+						+ "lambdaSafeDetected DOUBLE,"
+						+ "lambdaSafeUndetected DOUBLE,"
+						+ "lambdaUnSafeDetected DOUBLE,"
+						+ "lambdaUnSafeUndetected DOUBLE,"
+						+ "primary key(id));"
+						);
+				
+				System.out.println("components Tabelle erstellt");
+				
+				
+							
+			//inserting some standard components into the tables
+				
+				Component optokoppler = new Component("Optokoppler", 1, "used to transfer signals between two devided circuits");
+				optokoppler.addFailureMode("Unterbrechung Sender", 0.25, "Fehler wird durch fehlende Dynamik erkannt", 1.00, "", 0.1, 0.0, 0.0, 0.0);
+				optokoppler.addFailureMode("Kurzschluss Sender", 0.25, "Fehler wird durch fehlende Dynamik erkannt", 1.00, "", 0.1, 0.0, 0.0, 0.0);
+				optokoppler.addFailureMode("Unterbrechung Empfänger", 0.25, "Fehler wird durch fehlende Dynamik erkannt", 1.00, "", 0.1, 0.0, 0.0, 0.0);
+				optokoppler.addFailureMode("Kurzschluss Empfänger", 0.25, "Fehler wird durch fehlende Dynamik erkannt", 1.00, "", 0.1, 0.0, 0.0, 0.0);
+				optokoppler.addFailureMode("Kurzschluss Eingang, Ausgang", 0.25, "Fehler wird durch fehlende Dynamik erkannt", 1.00, "", 0.1, 0.0, 0.0, 0.0);
+				optokoppler.addFailureMode("Drift", 0.25, "Fehler wird durch fehlende Dynamik erkannt", 1.00, "", 0.1, 0.0, 0.0, 0.0);
+				
+				
+				
+				
+				//Adding Component
+				
+				addComponent(optokoppler);
 				
 						
-				PreparedStatement prep2 = con.prepareStatement("INSERT INTO components values(?,?,?,?);");
-				prep2.setInt(2, 2);
-				prep2.setString(3, "Treiber");
-				prep2.setString(4, "Unterbrechung Eingang");
-				prep2.execute();
-				
-				System.out.println("Keine FailureMode Tabelle gefunden, erstelle neue Tabelle");
-				Statement state3 = con.createStatement();
-				state3.execute("CREATE TABLE failureMode(id interger,"
-						+ "partOf varchar(60)," + "failerModeDesription varchar(500)," +"overAll double," + "measures carchar(500)," + "dc double," + "comment varchar(500)," + "lsD double," + "lSu double," + "lDd double," + "lDu double,"
-						+ "primary key(id));");
-				System.out.println("FailureMode erstellt");
-				
-				
-				
-				System.out.println("Dummies erstellt");
+			
 				
 			}
 		
@@ -99,12 +131,12 @@ public class SQLiteTest {
 	
 	
 	public void addComponent(String comptype, String failureMode) throws ClassNotFoundException, SQLException {
-		if (con == null) {
+		if (connection == null) {
 			getConnection();
 		
 		}
 		
-		PreparedStatement prep = con.prepareStatement("INSTERT INTO user values(?,?,?);");
+		PreparedStatement prep = connection.prepareStatement("INSTERT INTO user values(?,?,?);");
 		prep.setString(2, comptype);
 		prep.setString(3, failureMode);
 		prep.execute();
@@ -112,6 +144,33 @@ public class SQLiteTest {
 	}
 	
 	
-	
+	public void addComponent(Component component) throws SQLException {
+		
+		
+		PreparedStatement prepComponentStatement = connection.prepareStatement("INSERT INTO components values(?,?,?,?);");
+		prepComponentStatement.setInt(2, component.componentNumber);
+		prepComponentStatement.setString(3, component.componentType);
+		prepComponentStatement.setString(4, component.componentFunction);
+		prepComponentStatement.execute();
+		
+		for(FailureMode f : component.failuremodeList) {
+			
+			System.out.println("Adde failuremodes");
+			
+			PreparedStatement prepFailureModeStatement = connection.prepareStatement("INSERT INTO failuremodes values(?,?,?,?,?,?,?,?,?,?);");
+			prepFailureModeStatement.setString(2, component.componentType);
+			prepFailureModeStatement.setDouble(4, f.overAllProportion);
+			prepFailureModeStatement.setString(5, "measures");
+			prepFailureModeStatement.setDouble(6, f.dc);
+			prepFailureModeStatement.setDouble(7, f.getLambdaSd());
+			prepFailureModeStatement.setDouble(8, f.getLambdaSu());
+			prepFailureModeStatement.setDouble(9, f.getLambdaDd());
+			prepFailureModeStatement.setDouble(10, f.getLambdaDu());
+			
+		}
+		
+		
+		
+	}
 	
 }
